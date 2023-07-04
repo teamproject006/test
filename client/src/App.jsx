56f2eaae71849eca4fe96b5a2ef35ec10f20e6f0
Rafik006 <rafikmhadhbi01@gmail.com>
2023-07-04 14:05:39 +0100

    setup-folders

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FirstChild from "./components/firstChild/FirstChild.jsx";
+
+
+function App() {
+  return (
+    <div className="App">
+        parent component
+        <FirstChild />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/firstChild/FirstChild.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstChild = () => {
+  return (
+    <div>
+      child
+    </div>
+  )
+}
+
+export default FirstChild
