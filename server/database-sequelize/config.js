ea764a5a0c4e79b90f7b1f7c96e31aeddf9d262a
fares boutriga <boutrigafares@gmail.com>
2023-07-09 14:26:00 +0100

    change the model of services to acept many images

--- server/database-sequelize/config.js
+++ server/database-sequelize/config.js
@@ -1,14 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('rentService', 'fares', 'f@res1999', {
+
+const sequelize = new Sequelize('rentService', 'root', 'firemanfireman@@', {
   host: 'localhost',
   dialect: 'mysql',
 });
-// const sequelize = new Sequelize('rentService', 'root', 'firemanfireman@@', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-// });
 
 
 
--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use(cookieParser())
 
 app.use(cors({
   origin:["http://localhost:3000"],
-  methods:["POST,GET"],
   credentials:true
 }))
 
